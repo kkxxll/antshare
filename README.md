81cc671063427a5b194397c1890b15214fa6cb26
kkxxll <550893685@qq.com>
2018-09-15 10:31:14 +0800

    add antd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - create-react-app -g
 - react-router-dom axios less-loader less
 - yarn eject (export webpack config)
+- antd
--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import 'antd/dist/antd.css'
+
 import './App.less';
 
 class App extends Component {
