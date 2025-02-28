67556428bf32deac9cad865c7601e49b2d245f00
kaixin <kkxx.ll@qq.com>
2025-02-28 16:54:55 +0800

    1.vite 2.button

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import "./styles/reboot.scss"
+import "./App.scss";
+import Button from "./components/Button/button";
+function App() {
+  return (
+    <>
+      <div>
+        <Button >sfds</Button>
+        <Button btnType="primary">sfds</Button>
+        <Button btnType="danger" disabled>sfds</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Admin from './admin';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Admin />, document.getElementById('root'));
-registerServiceWorker();
