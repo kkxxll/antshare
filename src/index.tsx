798515c17e81611764e002e4e5f4a383a0faea69
kaixin <kkxx.ll@qq.com>
2025-03-13 22:43:44 +0800

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+
 export { default as Upload } from "./components/Upload";
 export { default as Button } from "./components/Button";
 export { default as Menu } from "./components/Menu";
